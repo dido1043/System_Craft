58015706903fd2606669002703ed5dbd67ea8a0b
dido1043 <dudunovski_05@abv.bg>
2025-11-10 21:13:52 +0200

    Hide .env and commit

--- SystemCraftAPI/obj/Debug/net9.0/SystemCraftAPI.AssemblyInfo.cs
+++ SystemCraftAPI/obj/Debug/net9.0/SystemCraftAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SystemCraftAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8bd9991334143fde4d6d330b59e7eaeb00740b86")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b48fcfb89ad89009cca5afeaba578a3ae83c58ae")]
 [assembly: System.Reflection.AssemblyProductAttribute("SystemCraftAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SystemCraftAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
